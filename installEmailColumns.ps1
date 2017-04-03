eb1b96b36e996ebea2c5655c0cf8fa1c3b46ad57
Colin Wood <cwood@opstest.local>
2017-04-03 20:38:55 +1000

    made changes to powershell

--- installEmailColumns.ps1
+++ installEmailColumns.ps1
@@ -5,19 +5,19 @@
         and then applies the email columns template to this site collection        
 #>
 
-try {
+try {    
     Set-ExecutionPolicy Bypass -Scope Process       
     $SharePointUrl = Read-Host -Prompt 'Enter your SharePoint Site Collection Url to install OnePlace Solutions Email Columns to'
     Connect-pnpOnline -url $SharePointUrl
-    $WebClient = New-Object System.Net.WebClient
-   
+    #Download xml provisioning template
+    $WebClient = New-Object System.Net.WebClient   
     $Url = "https://raw.githubusercontent.com/OnePlaceSolutions/EmailColumnsPnP/master/email-columns.xml"    
     $Path = "$env:temp\emailcolumns.xml"
+
     Write-Host "Downloading provisioning xml template:" $Path -ForegroundColor Green 
-    $WebClient.DownloadFile( $Url, $Path ) 
-  
-    #(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/OnePlaceSolutions/EmailColumnsPnP/master/email-columns.xml', '.\EmailColumns.xml')
-    Write-Host "Applying email columns template to SharePoint:" $Url -ForegroundColor Green 
+    $WebClient.DownloadFile( $Url, $Path )   
+    #Applyxml provisioning template to SharePoint
+    Write-Host "Applying email columns template to SharePoint:" $SharePointUrl -ForegroundColor Green 
     Apply-PnPProvisioningTemplate -path $Path
    
 }
